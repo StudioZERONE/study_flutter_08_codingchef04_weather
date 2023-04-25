ed33310360b9c0606cda0d23c8e88a0879ebf36c
ZERONE <zerone012@gmail.com>
2023-04-25 10:38:29 +0900

    http 패키지 및 날씨정보 가져오기

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -9,14 +10,30 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation(); //사용자가 앱을 실행하면, 즉시 위치 정보(경위도좌표)를 가져옴
+    fetchData();
+  }
+
   void getLocation() async {
     LocationPermission permission = await Geolocator.requestPermission();
 
-    Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-    );
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      print(position);
+    } catch (e) {
+      print('네트웍이 불안정합니다.');
+    }
+  }
 
-    print(position);
+  void fetchData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+    print(response.body);
   }
 
   @override
@@ -27,10 +44,7 @@ class _LoadingState extends State<Loading> {
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            getLocation();
-            print('Pressed');
-          },
+          onPressed: null,
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.blue,
             foregroundColor: Colors.white,
